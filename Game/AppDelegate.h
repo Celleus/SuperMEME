5e3095eff4a1dfdb8cc34fd43f05a3859ebcfd15
Celleus <celleus@celleus-no-macbook-pro.local>
2016-05-26 17:48:51 +0900

    Initial Commit

---
+++ Game/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Game
+//
+//  Created by Celleus on 2016/05/26.
+//  Copyright © 2016年 Celleus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Game/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Game
+//
+//  Created by Celleus on 2016/05/26.
+//  Copyright © 2016年 Celleus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
